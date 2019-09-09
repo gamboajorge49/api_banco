89892394c0ee5a17300d41ca81c0f133a2ca3754
Jorge Gamboa <gamboajorge49@gmail.com>
2019-09-09 20:33:18 -0300

    Adição da classe SecurityConfig, para gerir a autenticação oauth e gerar o token; Configuração do usuario de senha do client; Adição de um usuario 'inMemoryAuthentication';

--- banco/src/main/java/com/jorge/banco/repository/ExtratoRepository.java
+++ banco/src/main/java/com/jorge/banco/repository/ExtratoRepository.java
@@ -1,12 +1,17 @@
 package com.jorge.banco.repository;
 
+import java.util.List;
+
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.jorge.banco.model.Extrato;
 
 public interface ExtratoRepository extends JpaRepository<Extrato,Integer> {
 
-	
-	
+	/*@Query(value="select e from Extrato e where e.id_conta = :id_conta")
+    public List<Extrato> findAll(@Param("id_conta") int id_conta);*/
 	
 }
